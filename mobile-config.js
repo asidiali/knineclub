2153115d1fb0ee3128bc8d715b3dfda3ed295df3
Adam Sidiali <adamsidiali@gmail.com>
2015-10-10 11:06:29 -0700

    new structure

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  name: 'App Name',
+  description: 'An app.',
+  version: '1.0'
+});
+
+App.icons({
+  'android_ldpi': 'public/logo.png',
+  'android_mdpi': 'public/logo.png',
+  'android_hdpi': 'public/logo.png',
+  'android_xhdpi': 'public/logo.png'
+});
